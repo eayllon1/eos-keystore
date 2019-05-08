d57c1683d8776da0184a857e5e5d7dc92fcc46f8
eayllon1 <eayllon1@gmail.com>
2019-05-07 20:06:36 -0400

    added keystorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,125 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  AsyncStorage
+} from 'react-native';
+import { Api, JsonRpc, RpcError } from 'eosjs';
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
 import Button from './components/Button';
-import AccountCreator from './components/AccountCreator';
+import Input from './components/Input';
+
+const rpc = new JsonRpc('http://127.0.0.1:8888', { fetch });
 
 export default class App extends React.Component {
+  state = {
+    privateKey: '',
+    loggedIn: false,
+  }
+
+  onInputChange = (key, value) => {
+    this.setState(prevState => ({
+      ...prevState,
+      [key]: value
+    }))
+  }
+
+  retrieveItem = async (key) => {
+    try {
+      const retrievedItem =  await AsyncStorage.getItem(key);
+      const item = JSON.parse(retrievedItem);
+      return item;
+    } catch (error) {
+      console.log(error.message);
+    }
+    return
+  }
+
+  storeItem = async (key, store) => {
+    try {
+      const privatekey = await AsyncStorage.setItem(key, JSON.stringify(store));
+      return privatekey
+    } catch (err) {
+      console.log(err.message)
+    }
+  };
+
+  removeItem = async (key) => {
+    try {
+      await AsyncStorage.removeItem(key);
+      return true;
+    }
+    catch(exception) {
+      return false;
+    }
+  }
+
+  triggerStoreKey = async () => {
+    await this.storeItem("PRIVATE_KEY", this.state.privateKey);
+    this.setState({
+      loggedIn: true
+    })
+  }
+
+  triggerRemoveKey = async () => {
+    let success = this.removeItem("PRIVATE_KEY");
+    if (success) {
+      this.setState({
+        loggedIn: false,
+        privateKey: ''
+      })
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      let privkey = await this.retrieveItem("PRIVATE_KEY");
+      console.log(privkey);
+      if (privkey != '' && privkey != null) {
+        this.setState({
+          privateKey: privkey,
+          loggedIn: true
+        });
+      }
+    } catch (error) {
+      console.log('Promise is rejected with error: ' + error);
+    }
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
         <Text style = {{fontSize: 20, marginBottom: 20}}>
           EOS Keystore
         </Text>
+
+        { this.state.loggedIn ? (
+          <View>
+            <Button
+              title='Remove Privatekey'
+              onPress={this.triggerRemoveKey}
+             />
+          </View>
+
+        ) : (
+          <View>
+            <Input
+              placeholder="Enter Private Key"
+              type='privateKey'
+              name='privateKey'
+              onChangeText={this.onInputChange}
+              value={this.state.privateKey}
+            />
+            <Button
+              title='Load Account'
+              onPress={this.triggerStoreKey}
+             />
+          </View>
+        )}
+
+
       </View>
     );
   }
