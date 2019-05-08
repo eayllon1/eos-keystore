f934c16e4ac99965b328b40ee4f63b7f3e02bf6e
eayllon1 <eayllon1@gmail.com>
2019-05-07 20:39:31 -0400

    sends a payment

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ export default class App extends React.Component {
   state = {
     privateKey: '',
     loggedIn: false,
+    from: '',
+    to: ''
   }
 
   onInputChange = (key, value) => {
@@ -38,7 +40,7 @@ export default class App extends React.Component {
 
   storeItem = async (key, store) => {
     try {
-      const privatekey = await AsyncStorage.setItem(key, JSON.stringify(store));
+      const privatekey = await AsyncStorage.setItem(key, store);
       return privatekey
     } catch (err) {
       console.log(err.message)
@@ -58,7 +60,8 @@ export default class App extends React.Component {
   triggerStoreKey = async () => {
     await this.storeItem("PRIVATE_KEY", this.state.privateKey);
     this.setState({
-      loggedIn: true
+      loggedIn: true,
+      privateKey: ''
     })
   }
 
@@ -72,13 +75,52 @@ export default class App extends React.Component {
     }
   }
 
+  sendTransfer = async () => {
+    try {
+      const defaultPrivateKey = await AsyncStorage.getItem("PRIVATE_KEY");
+      console.log(defaultPrivateKey)
+      const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
+
+      const api = new Api({
+        rpc,
+        signatureProvider,
+        textDecoder: new TextDecoder(),
+        textEncoder: new TextEncoder()
+      });
+
+      const result = await api.transact({
+        actions: [{
+          account: 'eosio.token',
+          name: 'transfer',
+          authorization: [{
+            actor: this.state.from,
+            permission: 'active',
+          }],
+          data: {
+            from: this.state.from,
+            to: this.state.to,
+            quantity: this.state.amount,
+            memo: this.state.memo,
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 30,
+      });
+      console.log(result);
+    } catch (e) {
+      console.log('\nCaught exception: ' + e);
+      if (e instanceof RpcError)
+      console.log(JSON.stringify(e.json, null, 2));
+    }
+  }
+
   async componentDidMount() {
     try {
       let privkey = await this.retrieveItem("PRIVATE_KEY");
       console.log(privkey);
       if (privkey != '' && privkey != null) {
         this.setState({
-          privateKey: privkey,
           loggedIn: true
         });
       }
@@ -91,18 +133,53 @@ export default class App extends React.Component {
     console.log(this.state);
     return (
       <View style={styles.container}>
-        <Text style = {{fontSize: 20, marginBottom: 20}}>
+        <Text style = {{fontSize: 20, marginBottom: 20, fontWeight: '600'}}>
           EOS Keystore
         </Text>
 
         { this.state.loggedIn ? (
+
           <View>
+            <Text style = {{fontSize: 16, marginBottom: 20, fontWeight: '600'}}>
+              Send Payment
+            </Text>
+            <Input
+              placeholder="from"
+              type='from'
+              name='from'
+              onChangeText={this.onInputChange}
+              value={this.state.from}
+            />
+            <Input
+              placeholder="to"
+              type='to'
+              name='to'
+              onChangeText={this.onInputChange}
+              value={this.state.to}
+            />
+            <Input
+              placeholder="amount"
+              type='amount'
+              name='amount'
+              onChangeText={this.onInputChange}
+              value={this.state.amount}
+            />
+            <Input
+              placeholder="memo"
+              type='memo'
+              name='memo'
+              onChangeText={this.onInputChange}
+              value={this.state.memo}
+            />
+            <Button
+              title='Send Payment'
+              onPress={this.sendTransfer}
+             />
             <Button
               title='Remove Privatekey'
               onPress={this.triggerRemoveKey}
              />
           </View>
-
         ) : (
           <View>
             <Input
